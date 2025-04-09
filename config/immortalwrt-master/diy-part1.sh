a3906e5c33c6e4f27863120b61b115a69df842af
tianteng888 <3200300473@qq.com>
2025-04-10 06:55:44 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -7,13 +7,13 @@
 #========================================================================================================================
 
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git lienol https://github.com/zwrt/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git lienol https://github.com/openwrt/packages' feeds.conf.default
-# sed -i '$a src-git lienol https://github.com/NueXini/NueXini_Packages' feeds.conf.default
-# sed -i '$a src-git lienol http://downloads.openwrt.org/releases/24.10.0/packages/aarch64_cortex-a53/packages/' feeds.conf.default
-# sed -i '$a src-git lienol https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.0/packages/aarch64_cortex-a53/packages/' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/zwrt/openwrt-packages' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/openwrt/packages' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/NueXini/NueXini_Packages' feeds.conf.default
+sed -i '$a src-git lienol http://downloads.openwrt.org/releases/24.10.0/packages/aarch64_cortex-a53/packages/' feeds.conf.default
+sed -i '$a src-git lienol https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.0/packages/aarch64_cortex-a53/packages/' feeds.conf.default
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 # other
