40af2434f6a863eecedb5ed42bfbf0f5be1e8e17
tianteng888 <3200300473@qq.com>
2025-04-11 00:48:45 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -5,8 +5,10 @@
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
 # Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
 #========================================================================================================================
+# 清理重复的 lienol 源 
+sed -i '/lienol/d' feeds.conf.default
 
-# Add a feed source
+# 添加源
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/zwrt/openwrt-packages' feeds.conf.default
@@ -14,13 +16,10 @@ sed -i '$a src-git lienol https://github.com/openwrt/packages' feeds.conf.defaul
 sed -i '$a src-git lienol https://github.com/NueXini/NueXini_Packages' feeds.conf.default
 sed -i '$a src-git lienol http://downloads.openwrt.org/releases/24.10.0/packages/aarch64_cortex-a53/packages/' feeds.conf.default
 sed -i '$a src-git lienol https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.0/packages/aarch64_cortex-a53/packages/' feeds.conf.default
-echo >> feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 
 # 添加 iStore 官方仓库
 echo 'src-git istore https://github.com/linkease/istore.git;main' >> feeds.conf.default
 
-# other
+# 其他
 # rm -rf package/emortal/{autosamba,ipv6-helper}
-
 
